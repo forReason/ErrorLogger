38f6ae224dd6e7c63635ce635b76c0150912c1a4
Julian B <bechti44@googlemail.com>
2023-02-26 23:01:29 +0100

    fixed naming issue

---
+++ TestingApplication/Program.cs
@@ -0,0 +1,9 @@
+﻿using ErrorLogger;
+
+var logger = new Logger(
+                filePath: "C:\\logs\\error.csv",
+                historyDuration: TimeSpan.FromDays(30.0),
+                maxLogSize_KB: 1024
+                );
+// Act
+throw new DataMisalignedException("the data is misaligned!");
